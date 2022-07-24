d730378530e4f6acc0401602fa97d6841a5d5a87
jisena.btc <pjohmaths@gmail.com>
2022-07-24 23:49:22 +0300

    add vscode dark theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      black: "#1e1e1e",
+      light: "#d4d4d4",
+      divider: "#007acc"
+    },
     extend: {},
   },
   plugins: [],
