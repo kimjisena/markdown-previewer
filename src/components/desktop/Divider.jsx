93b76783bcee447ca63c29c92fd577169a68daf8
jisena.btc <pjohmaths@gmail.com>
2022-07-25 08:06:41 +0300

    user stories tests passing

--- src/components/desktop/Divider.jsx
+++ src/components/desktop/Divider.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Divider() {
 
   return (
-    <div className={`fixed left-[49.8%] h-full w-[0.4%] top-[10vh] bg-blue hover:cursor-col-resize` }>
+    <div className={`fixed left-[49.8%] h-full w-[0.4%] top-[10vh] bg-blue` }>
     </div>
   );
 }
