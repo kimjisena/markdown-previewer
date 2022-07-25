5eea55660fcb481893385af6f786adfd328245d8
jisena.btc <pjohmaths@gmail.com>
2022-07-25 03:23:01 +0300

    implement sidebar

--- src/components/desktop/Editor.jsx
+++ src/components/desktop/Editor.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import {IoMdInformationCircle} from 'react-icons/io';
 import {GoPrimitiveDot} from 'react-icons/go';
+import {VscFiles} from 'react-icons/vsc';
+import {VscSearch} from 'react-icons/vsc';
+import {VscSourceControl} from 'react-icons/vsc';
+import {VscDebugAlt} from 'react-icons/vsc';
+import {VscExtensions} from 'react-icons/vsc';
+import {VscAccount} from 'react-icons/vsc';
+import { VscSettingsGear } from 'react-icons/vsc';
 
 const placeholder = `Reprehenderit non adipisicing dolor pariatur. Adipisicing non proident id officia exercitation consequat amet incididunt dolore incididunt velit reprehenderit voluptate Lorem. Et nulla aute id mollit.
 
@@ -15,7 +22,20 @@ Aliqua sint duis aute Lorem consectetur proident esse labore labore ex cillum. V
 function Editor() {
   return (
     <div className={`fixed left-0 w-[49.8%] h-[90vh] top-[10vh]`}>
-        <div className={`bg-subhead h-[5vh] text-subheadText text-xs`}>
+        <div className={`absolute left-0 h-full w-[8%] bg-subhead flex flex-col justify-between`}>
+            <div className={`justify-self-start h-[40%] flex flex-col justify-evenly items-center`}>
+                <VscFiles size={`24px`} />
+                <VscSearch size={`24px`} />
+                <VscSourceControl size={`24px`} />
+                <VscDebugAlt size={`24px`} />
+                <VscExtensions size={`24px`} />
+            </div>
+            <div className={`justify-self-end h-[20%] flex flex-col justify-evenly items-center`}>
+                <VscAccount size={`24px`} />
+                <VscSettingsGear size={`24px`} />
+            </div>
+        </div>
+        <div className={`bg-subhead h-[5vh] text-subheadText text-xs px-[8%]`}>
             <div className={'flex h-full bg-black w-[20%] justify-evenly items-center'}>
                 <IoMdInformationCircle className={`bg-black text-blue`} size={`16px`}/>
                 <h1>
@@ -24,7 +44,7 @@ function Editor() {
                 <GoPrimitiveDot className={`text-[white]`} size={`16px`} />
             </div>
         </div>
-        <textarea id="editor" className={`w-full h-full border-0 bg-black`} value={placeholder}>
+        <textarea id="editor" className={`w-full h-full border-0 bg-black px-[8%]`} value={placeholder}>
         </textarea>
     </div>
   );
